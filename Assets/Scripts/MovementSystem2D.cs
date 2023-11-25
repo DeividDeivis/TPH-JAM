c29653f9bbcdeff95082935646c2c24c816ea637
David Ruiz <deivid.ruiz93@gmail.com>
2023-11-25 03:14:13 -0300

    Player Assets + Animator + Player Controller  + Movement System

--- Assets/Scripts/MovementSystem2D.cs
+++ Assets/Scripts/MovementSystem2D.cs
@@ -1,24 +1,19 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class MovementSystem2D : MonoBehaviour
 {
 	[SerializeField] private float m_JumpForce = 400f;
-	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;      // Velocidad de movimiento agachado. 1 = 100%
-	[Range(1, 2)] [SerializeField] private float m_RunSpeed = 1.3f;
 	[Range(0, .3f)] [SerializeField] private float m_SpeedSmooth = .05f;    // Suavizado del movimiento.
 	[SerializeField] public bool m_AirControl = false;
-	[SerializeField] private LayerMask m_WhatIsGround;							// Layer del suelo donde el player se apoya.
-	[SerializeField] private Transform m_GroundCheck;							// Verifica si esta en contacto con el suelo.
-	[SerializeField] private Transform m_CeilingCheck;							// Verifica si esta en contacto con un techo.
-	[SerializeField] private Collider m_StandingCollider;						// El collider se deshabilita cuando esta agachado.
+	[SerializeField] private LayerMask m_WhatIsGround;						// Layer del suelo donde el player se apoya.
+	[SerializeField] private Transform m_GroundCheck;                       // Verifica si esta en contacto con el suelo.
 
-	const float k_GroundCheckRadio = .2f; 	// Radio del circulo que detecta el Suelo.
+    [SerializeField] private float k_GroundCheckRadio = .2f; 	// Radio del circulo que detecta el Suelo.
 	public bool m_InGround;             	// Esta tocando el Suelo?.
-	const float k_CeilingCheckRadio = .2f;    // Radio del circulo que detecta si esta chocando con un techo. No permite salto o pararse.
-	private Rigidbody m_Rigidbody;		// Cuerpo rigido del Player.
-	public bool m_SpriteRight = true;  	// Esta el sprite en la posicion original?(mirando a la derecha).
+	private Rigidbody2D m_Rigidbody;			// Cuerpo rigido del Player.
+	public bool m_SpriteRight = true;  		// Esta el sprite en la posicion original?(mirando a la derecha).
 	private Vector3 m_Velocity = Vector3.zero;
 
 	[Header("Events")]
@@ -26,21 +21,13 @@ public class MovementSystem2D : MonoBehaviour
 
 	public UnityEvent InGround;
 
-	[System.Serializable]
-	public class BoolEvent : UnityEvent<bool> { }
-
-	public BoolEvent IsCrounch;
-	private bool m_IsCrouch = false;
 
 	private void Awake()
 	{
-		m_Rigidbody = GetComponent<Rigidbody>();
+		m_Rigidbody = GetComponent<Rigidbody2D>();
 
 		if (InGround == null)
 			InGround = new UnityEvent();
-
-		if (IsCrounch == null)
-			IsCrounch = new BoolEvent();
 	}
 
 	private void FixedUpdate()
@@ -48,70 +35,23 @@ public class MovementSystem2D : MonoBehaviour
 		bool m_IsGrounded = m_InGround;
 		m_InGround = false;
 
-		Collider[] colliders = Physics.OverlapSphere(m_GroundCheck.position, k_GroundCheckRadio, m_WhatIsGround); // Detecta si SueloChack detecta Suelo.
-		for (int i = 0; i < colliders.Length; i++)
+		Collider2D collider = Physics2D.OverlapCircle(m_GroundCheck.position, k_GroundCheckRadio, m_WhatIsGround); // Detecta si SueloChack detecta Suelo.
+
+		if (collider != null && collider.gameObject.tag == "Ground")
 		{
-			if (colliders[i].gameObject != gameObject)
-			{
-				m_InGround = true;
-				if (!m_IsGrounded)
-					InGround.Invoke();
-			}
-		}
+			m_InGround = true;
+			if (!m_IsGrounded)
+				InGround.Invoke();
+		}		
 	}
 
 
-	public void Move(float move, bool run,bool crouch, bool jump)
+	public void Move(float move, bool jump)
 	{
-		// If crouching, check to see if the character can stand up
-		if (!crouch)
-		{
-			// If the character has a ceiling preventing them from standing up, keep them crouching
-			if (Physics.OverlapSphere(m_CeilingCheck.position, k_CeilingCheckRadio, m_WhatIsGround).Length > 0)
-			{
-				crouch = true;
-			}
-		}
 
 		//only control the player if grounded or airControl is turned on
 		if (m_InGround || m_AirControl)
 		{
-
-			// If crouching
-			if (crouch && !run)
-			{
-				if (!m_IsCrouch)
-				{
-					m_IsCrouch = true;
-					IsCrounch.Invoke(true);
-				}
-
-				// Reduce the speed by the crouchSpeed multiplier
-				move *= m_CrouchSpeed;
-
-				// Disable one of the colliders when crouching
-				if (m_StandingCollider != null)
-					m_StandingCollider.enabled = false;
-			} else
-			{
-				// Enable the collider when not crouching
-				if (m_StandingCollider != null)
-					m_StandingCollider.enabled = true;
-
-				if (m_IsCrouch)
-				{
-					m_IsCrouch = false;
-					IsCrounch.Invoke(false);
-				}
-
-				if (run) 
-				{
-					move *= m_RunSpeed;
-					m_IsCrouch = false;
-					IsCrounch.Invoke(false);
-				}
-			}
-
 			// Move the character by finding the target velocity
 			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody.velocity.y);
 			// And then smoothing it out and applying it to the character
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Spine.Unity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,118 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private float m_MovementX;
-    private bool m_IsJump;
+    // Controllers
+    [SerializeField] private MovementSystem2D Movecontroller; // Se usa el Script Plataforma2D como referencia.	
 
-    public void OnMove(InputAction.CallbackContext context) 
+    [Header("Character Stats")]
+    [SerializeField] private float moveSpeed = 40f;
+
+    [Header("Animations Parameters")]
+    [SerializeField] private Animator animator;
+    [SerializeField] private float idleTime;
+    private float currentIdle;
+
+    [Header("Player Actions Permition")]
+    public bool movement; // movement = can player move?
+    [SerializeField] private bool m_IsJump;
+    private float m_MovementX;
+    private float horizontalMove = 0f;
+
+    /*[Header("Walk SFX")]
+    [SerializeField] private EventReference m_FootStepEventRef;
+    private EventInstance m_FootStepInstance;
+    [SerializeField] private float m_FootStepTime;
+    private float curretStep;
+    [Header("Damage SFX")]
+    [SerializeField] private EventReference m_DeadSfx;*/
+
+
+    void Start()
+    {
+        //m_FootStepInstance = RuntimeManager.CreateInstance(m_FootStepEventRef);
+        currentIdle = idleTime;
+    }
+
+    // Update is called once per frame    
+    void Update()
+    {
+        //animator.SetBool("EnSuelo", controller.m_EnSuelo);       
+
+        if (movement == true)
+        {   // Si el movimiento esta activado, habilita las animaciones.
+            horizontalMove = m_MovementX * moveSpeed; // Varia entre -1 y 1. Funciona para teclado o joystick, ver Conf del proyecto.
+            animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+            /*if (Mathf.Abs(horizontalMove) > 0)
+            {
+                FootStepSFX();
+                currentIdle = idleTime;
+            }
+            else
+                curretStep = 0;*/
+
+            //Idle Anim
+            if (currentIdle > 0)
+                currentIdle -= Time.deltaTime;
+            else
+            {
+                animator.SetTrigger("LookAround");
+                currentIdle = idleTime;
+            }
+        }
+    }
+
+    // Para las Fisicas, usamos Input para mover el personaje.
+    void FixedUpdate()
+    {
+        if (movement == true)
+        {
+            Movecontroller.Move(horizontalMove * Time.fixedDeltaTime, m_IsJump);
+            m_IsJump = false;
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
     {
         m_MovementX = context.ReadValue<float>();
     }
 
-    public void OnJump(InputAction.CallbackContext context) 
+    public void OnJump(InputAction.CallbackContext context)
     {
         //m_IsJump = context.ReadValue<bool>();
         m_IsJump = context.action.triggered;
+        currentIdle = idleTime;
     }
+
+    public void OnLanding()
+    {          
+        animator.SetBool("Jump", false);
+        animator.SetBool("Landing", true);
+    }
+
+    private void Die()
+    {
+        // Do Something.       
+    }
+
+    /*public void FootStepSFX()
+    {
+        curretStep -= Time.deltaTime;
+        if (curretStep <= 0)
+        {
+            if (run)
+            {
+                m_FootStepInstance.setParameterByNameWithLabel("Movement Speed", "Run");
+                m_FootStepInstance.start();
+                m_FootStepInstance.release();
+            }
+            else
+            {
+                m_FootStepInstance.setParameterByNameWithLabel("Movement Speed", "Walk");
+                m_FootStepInstance.start();
+                m_FootStepInstance.release();
+            }
+            curretStep = m_FootStepTime;
+        }
+    }*/
 }
