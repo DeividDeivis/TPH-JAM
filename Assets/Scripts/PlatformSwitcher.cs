eb098b1273d61f511ec098eab311ba40cc875820
David Ruiz <deivid.ruiz93@gmail.com>
2023-11-25 13:51:09 -0300

    DOTween + Player Animator  Update + Players Prefabs + Platform Controller + Platform Switcher

---
+++ Assets/Scripts/PlatformSwitcher.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSwitcher : MonoBehaviour
+{
+    [SerializeField] private PlatformController m_Platform;
+    [SerializeField] private StateType m_InitialState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum StateType { Active, Inactive }
